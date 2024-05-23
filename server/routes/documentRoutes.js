4c89af487c10d14a7966174cc0a240aed6fc3150
nhattytw <nhatty567@outlook.com>
2024-05-23 14:16:36 +0300

    server init

---
+++ server/routes/documentRoutes.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+const authMiddleware = require('../middleware/authMiddleware')
+const upload = require('../config/multerConfig')
+const fileErrorHandler = require('../middleware/fileErrorHandler')
+const uploadRateLimiter = require('../middleware/rateLimiter')
+// const uploadAndScanDocument = require('../middleware/uploadAndScanDocument')
+const {
+      getDocuments,
+      uploadDocument,
+      downloadDocument,
+      deleteDocument,
+      editDocumentName } = require('../controllers/documentController')
+
+router.get(
+      '/',
+      authMiddleware,
+      getDocuments,
+      fileErrorHandler
+)
+
+router.post(
+      '/upload',
+      authMiddleware,
+      upload,
+      // uploadAndScanDocument,
+      fileErrorHandler,
+      uploadRateLimiter,
+      uploadDocument,
+)
+
+router.put(
+      '/edit',
+      authMiddleware,
+      editDocumentName,
+      fileErrorHandler
+)
+
+router.post(
+      '/download',
+      authMiddleware,
+      downloadDocument
+)
+
+router.delete(
+      '/delete',
+      authMiddleware,
+      deleteDocument
+)
+
+module.exports = router
